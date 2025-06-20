29fad8d8524e4c7f0995ac2d5df8137c526fb97d
Atul Kahate <newdelthis@gmail.com>
2025-06-20 15:26:26 +0530

    modified app.py to add __main__ etc

--- code/app.py
+++ code/app.py
@@ -11,3 +11,6 @@ def predict():
     experience = np.array(data["experience"]).reshape(-1, 1)
     prediction = model.predict(experience)
     return jsonify({"prediction": prediction.tolist()})
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=5000, debug=True)
